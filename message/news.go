b5b34407df6c3f2070d90314c349ef7725460d8a
hsx <shunxin.hua@verystar.cn>
2018-11-07 16:39:53 +0800

    news msg support cdata

--- message/news.go
+++ message/news.go
@@ -17,24 +17,24 @@ type News struct {
 func NewNews(MpArticle []*MpArticle) *News {
 	articleContent := make([]*Article, 0)
 	for _, value := range MpArticle {
-		a := &Article{}
-		a.URL = CDATA{Value: value.URL}
-		a.Title = CDATA{Value: value.Title}
-		a.Description = CDATA{Value: value.Description}
-		a.PicURL  = CDATA{Value: weixin.StaticHost + value.PicURL + weixin.OssThumbNail}
+		a 			  := &Article{}
+		a.URL          = CDATA{Value: value.URL}
+		a.Title        = CDATA{Value: value.Title}
+		a.Description  = CDATA{Value: value.Description}
+		a.PicURL       = CDATA{Value: weixin.StaticHost + value.PicURL + weixin.OssThumbNail}
 		articleContent = append(articleContent, a)
 		break
 	}
 
 	news := new(News)
 	news.ArticleCount = 1 //微信升级之后, 只能是单图文
-	news.Articles = articleContent
+	news.Articles     = articleContent
 	return news
 }
 
 //Article 单篇文章 都是必传参数
 type Article struct {
-	Title       CDATA `xml:"Title" json:"Title1"`
+	Title       CDATA `xml:"Title" json:"Title"`
 	Description CDATA `xml:"Description" json:"Description"`
 	PicURL      CDATA `xml:"PicUrl" json:"PicUrl"`
 	URL         CDATA `xml:"Url" json:"Url"`
